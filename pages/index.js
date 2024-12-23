8c2752fb1597ec4506eba08358899c16550cddf1
Geórgia Moreira <georgiamoreira13@gmail.com>
2024-12-23 14:36:19 +0000

    estiliza frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Te amo maridinho Almeida, obrigada por ser meu maior incentivador e contribuidor do meu crescimento!</h1>
+    return <b><h1>Te amo maridinho <i>Almeida</i>, obrigada por ser meu maior incentivador e contribuidor do meu crescimento!</h1></b>
 }
 
 export default Home;
