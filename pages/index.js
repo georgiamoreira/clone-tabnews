33264de5e330d7f648cf2e52738c3e24429d321c
Geórgia Moreira <georgiamoreira13@gmail.com>
2025-01-06 15:58:53 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home() {
-    return <b><h1>Te amo maridinho <i>Almeida</i>, obrigada por ser meu maior incentivador e contribuidor do meu crescimento!</h1></b>
+  return (
+    <b>
+      <h1>
+        Te amo maridinho <i>Almeida</i>, obrigada por ser meu maior incentivador
+        e contribuidor do meu crescimento!
+      </h1>
+    </b>
+  );
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
